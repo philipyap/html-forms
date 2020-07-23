bb6ee55e41d7bbc2b78155047733b94521729779
Philip Yap <philipyap@Philips-MacBook.local>
2020-07-23 12:34:47 -0700

    [html-forms]add forms

---
+++ results.js
@@ -0,0 +1,24 @@
+const resultDiv = document.querySelector('#results');
+console.log(resultDiv);
+
+new URLSearchParams(window.location.search).forEach((value, name)=> {// window means browser
+        console.log(name);
+        console.log(value);
+        // let nameElement = document.createElement('p');
+        // let valueElement = document.createElement('p');
+
+        // nameElement.textContent = name;
+        // valueElement.textContent = value;
+
+        // console.log(nameElement);
+
+//         resultDiv.appendChild(nameElement);
+//         resultDiv.appendChild(valueElement);
+
+        let resultElement = document.createElement('p');
+        resultElement.textContent = name + ' ' + value;
+
+        resultDiv.appendChild(resultElement);
+
+}) 
+
